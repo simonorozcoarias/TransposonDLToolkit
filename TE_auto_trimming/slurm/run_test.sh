d13bb264ad147f9aab0f2668ccc9540cbba490f2
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2026-01-05 23:54:33 +0100

    bash script to test a model

---
+++ TE_auto_trimming/slurm/run_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -o test_model.out
+#SBATCH -e test_model.err
+#SBATCH --mail-type END
+#SBATCH --mail-user agonzalezpalo@uoc.edu
+#SBATCH -J test_model
+#SBATCH --time 1-00:00:00
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH --mem 200GB
+
+module load tensorflow-gpu/2.6.2
+
+/shared/ifbstor1/projects/tagua_gen_ec/anaconda3_homesimon/envs/autotrim_env/bin/python3.9 \
+    Auto_trimming.py \
+    --mode test \
+    --input_fasta ./data_generation/simulated_data_merged.fasta \
+    --model ./models/model_trained_9218/trained_model.h5 \
+    --scaler ./models/model_trained_9218/scalerX.bin \
+    --dataset_testing ./datasets/dataset_testing
