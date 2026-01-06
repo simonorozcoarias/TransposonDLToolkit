654af8a4b3a9d94dc6a2a750a9221021a0d19734
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2026-01-06 02:16:20 +0100

    Update run_test.sh

--- TE_auto_trimming/slurm/run_test.sh
+++ TE_auto_trimming/slurm/run_test.sh
@@ -15,7 +15,6 @@ module load tensorflow-gpu/2.6.2
 /shared/ifbstor1/projects/tagua_gen_ec/anaconda3_homesimon/envs/autotrim_env/bin/python3.9 \
     Auto_trimming.py \
     --mode test \
-    --input_fasta ./data_generation/simulated_data_merged.fasta \
     --model ./models/model_trained_9218/trained_model.h5 \
     --scaler ./models/model_trained_9218/scalerX.bin \
     --dataset_testing ./datasets/dataset_testing
